0812330e64cdf1a822c9442f4299dcd0b6b7ae12
brunodonatob <bruno.donato.banhos@gmail.com>
2017-06-02 12:02:01 -0300

    4 Objetos adicionados e rotação da câmera.

--- ex3/functions.js
+++ ex3/functions.js
@@ -1,10 +1,11 @@
 var camera, scene, renderer;
 var container;
-var objeto;
+var objeto1;
 var objeto2;
+var objeto3;
+var objeto4;
 
 init();
-animate();
 render();
 
 function init() {
@@ -13,8 +14,8 @@ function init() {
   document.body.appendChild(container);
 
   //camera
-  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 2000);
-  camera.position.set(0,70,10);
+  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 2000);
+  camera.position.set(25, 15, 55);
 
   //scene
   scene = new THREE.Scene();
@@ -27,10 +28,10 @@ function init() {
   container.appendChild(renderer.domElement);
 
   //plane for the ground
-  var planeGeometry = new THREE.PlaneBufferGeometry(0.0001, 0.0001, 0.0001);
-  var planeMaterial = new THREE.MeshBasicMaterial({color: null});
+  var planeGeometry = new THREE.PlaneBufferGeometry(0.001, 0.001, 0.001);
+  var planeMaterial = new THREE.MeshBasicMaterial();
   var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-  plane.position.set(0,0,0);
+  plane.position.set(0, 10, 0);
   scene.add(plane);
 
   //ambient light
@@ -39,70 +40,93 @@ function init() {
 
   //set another light
   var spotLight = new THREE.SpotLight(0xffffff);
-  spotLight.position.set(100,-100,100);
+  spotLight.position.set(10,-100, 100);
   spotLight.castShadow = true;
-  spotLight.shadowMapWidth = 1024;
-  spotLight.shadowMapHeight = 1024;
-  spotLight.shadowCameraNear = 500;
-  spotLight.shadowCameraFar = 4000;
-  spotLight.shadowCameraFov = 30;
+  spotLight.shadow.mapSize.width = 1024;
+  spotLight.shadow.mapSize.height = 1024;
+  spotLight.shadow.camera.near = 500;
+  spotLight.shadow.camera.far = 4000;
+  spotLight.shadow.camera.fov = 30;
   scene.add(spotLight);
 
   //mtl = textura e cor
-  var mtlLoader = new THREE.MTLLoader();
-  mtlLoader.setPath('obj/');
-  mtlLoader.load('cosmo.mtl', function(materials) {
+  var mtlLoaderObjeto1 = new THREE.MTLLoader();
+  mtlLoaderObjeto1.setPath('obj/');
+  mtlLoaderObjeto1.load('wanda.mtl', function(materials) {
     materials.preload();
-    var objLoader = new THREE.OBJLoader();
-    objLoader.setMaterials(materials);
-    objLoader.setPath('obj/');
-    objLoader.load('cosmo.obj', function(object) {
+    var objeto1 = new THREE.OBJLoader();
+    objeto1.setMaterials(materials);
+    objeto1.setPath('obj/');
+    objeto1.load('wanda.obj', function(object) {
       object.scale.set(25,25,25);
-      object.position.set(20, 5, -5);
+      object.position.set(-20, 0, 15);
       scene.add(object);
-      objeto = object;
     });
   });
 
   //mtl = textura e cor
-  var mtlLoader2 = new THREE.MTLLoader();
-  mtlLoader.setPath('obj/');
-  mtlLoader.load('wanda.mtl', function(materials) {
+  var mtlLoaderObjeto2 = new THREE.MTLLoader();
+  mtlLoaderObjeto2.setPath('obj/');
+  mtlLoaderObjeto2.load('cosmo.mtl', function(materials) {
     materials.preload();
-    var objLoader2 = new THREE.OBJLoader();
-    objLoader2.setMaterials(materials);
-    objLoader2.setPath('obj/');
-    objLoader2.load('wanda.obj', function(object) {
+    var objeto2 = new THREE.OBJLoader();
+    objeto2.setMaterials(materials);
+    objeto2.setPath('obj/');
+    objeto2.load('cosmo.obj', function(object) {
       object.scale.set(25,25,25);
-      object.position.set(-20, 5, -5);
+      object.position.set(0, 0, 10);
       scene.add(object);
-      objeto2 = object;
+    });
+  });
+
+  //mtl = textura e cor
+  var mtlLoaderObjeto3 = new THREE.MTLLoader();
+  mtlLoaderObjeto3.setPath('obj/');
+  mtlLoaderObjeto3.load('wanda.mtl', function(materials) {
+    materials.preload();
+    var objeto3 = new THREE.OBJLoader();
+    objeto3.setMaterials(materials);
+    objeto3.setPath('obj/');
+    objeto3.load('wanda.obj', function(object) {
+      object.scale.set(25,25,25);
+      object.position.set(10, 0, -15);
+      scene.add(object);
+    });
+  });
+
+  //mtl = textura e cor
+  var mtlLoaderObjeto4 = new THREE.MTLLoader();
+  mtlLoaderObjeto4.setPath('obj/');
+  mtlLoaderObjeto4.load('cosmo.mtl', function(materials) {
+    materials.preload();
+    var objeto4 = new THREE.OBJLoader();
+    objeto4.setMaterials(materials);
+    objeto4.setPath('obj/');
+    objeto4.load('cosmo.obj', function(object) {
+      object.scale.set(25,25,25);
+      object.position.set(30, 0, -20);
+      scene.add(object);
     });
   });
 
   //camera look at
   camera.lookAt(plane.position);
-}
 
-function animate(){
-    requestAnimationFrame(animate);
-    render();
+
+  // HELPERS
+
+  // CAMERA HELPER
+  //var spotLightHelper = new THREE.SpotLightHelper( spotLight );
+  //scene.add( spotLightHelper );
+
+  // AXIS HELPER
+  //var axisHelper = new THREE.AxisHelper( 5 );
+  //scene.add(axisHelper);
 }
 
 function render(){
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
 
-    //movimento da camera
-    var timer = Date.now() * 0.0010;
-
-    camera.position.x = Math.cos(timer)*100;
-    camera.position.z = Math.sin(timer)*100;
-
-    camera.lookAt(scene.position);
-
-    //inicializar
-    requestAnimationFrame(render);
-    renderer.render(scene, camera);
-
-    objeto.rotation.y -= 0.001;
-    objeto2.rotation.y += 0.001;
+  scene.rotation.y += 0.01;
 }
